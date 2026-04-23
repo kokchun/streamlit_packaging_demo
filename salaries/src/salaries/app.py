66fb01dfb0ec7d3d6f918023a15d28586a345786
kokchun <kokchungiang@gmail.com>
2026-04-23 10:57:05 +0200

    dataframe in rawdata

--- salaries/src/salaries/app.py
+++ salaries/src/salaries/app.py
@@ -2,8 +2,8 @@ import streamlit as st
 
 pages = [
     st.Page("pages/home.py", title = "Home"),
-    st.Page("pages/raw_data.py", title = "Raw data"),
     st.Page("pages/dashboard.py", title = "Dashboard"),
+    st.Page("pages/raw_data.py", title = "Raw data"),
 ]
 
 pg = st.navigation(pages)
--- salaries/src/salaries/pages/raw_data.py
+++ salaries/src/salaries/pages/raw_data.py
@@ -1,3 +1,9 @@
 import streamlit as st 
+from salaries.utils.constants import DATA_PATH
+import pandas as pd 
+
+df = pd.read_csv(DATA_PATH / "salaries.csv")
 
 st.markdown("# RAW DATA")
+
+st.dataframe(df)
