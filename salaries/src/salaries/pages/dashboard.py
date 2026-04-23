7db0086a51fb02b5cbccf203481c665b49da2506
kokchun <kokchungiang@gmail.com>
2026-04-23 13:12:53 +0200

    kpis

--- salaries/src/salaries/pages/dashboard.py
+++ salaries/src/salaries/pages/dashboard.py
@@ -3,16 +3,33 @@ from salaries.utils.helpers import get_salaries_df, read_textfile
 from salaries.utils.constants import MARKDOWN_PATH
 from salaries.components.kpis import avg_salary_usd_kpi
 
+
 def dashboard_layout():
     st.markdown("# Salaries dashboard")
     st.markdown(read_textfile(MARKDOWN_PATH / "salaries_dashboard_description.md"))
     st.dataframe(get_salaries_df())
 
     # KPIs
-    avg_salary_usd_kpi("data analyst", "Data analyst")
+    roles = [
+        "Data Scientist",
+        "Data Analyst",
+        "Data Engineer",
+        "Machine Learning Engineer",
+        "AI Engineer",
+    ]
+
+    cols = st.columns(len(roles))
+
+    for column, role in zip(cols, roles):
+        with column:
+            avg_salary_usd_kpi(role, role)
+
+
+
     # data scientist
     # machine learning engineer
     # data engineer
+
 
 if __name__ == "__main__":
     dashboard_layout()
