1cfc5e5879fe6b43d50f838b6b9f366d711f8aee
kokchun <kokchungiang@gmail.com>
2026-04-23 10:28:33 +0200

    different paths in constants

---
+++ salaries/src/salaries/utils/constants.py
@@ -0,0 +1,12 @@
+from pathlib import Path
+
+BASE_PATH = Path(__file__).parents[1]
+
+ASSETS_PATH = BASE_PATH / "assets"
+COMPONENTS_PATH = BASE_PATH / "components"
+
+IMAGE_PATH = ASSETS_PATH / "image"
+STYLES_PATH = ASSETS_PATH / "style"
+MARKDOWN_PATH = ASSETS_PATH / "markdown"
+DATA_PATH = ASSETS_PATH / "data"
+
