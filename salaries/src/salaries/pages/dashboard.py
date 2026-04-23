49f19cdb7e8ea36b4f1c021ae27aef5e1213ff64
kokchun <kokchungiang@gmail.com>
2026-04-23 12:24:33 +0200

    building dashboard page

--- salaries/src/salaries/pages/dashboard.py
+++ salaries/src/salaries/pages/dashboard.py
@@ -1,3 +1,13 @@
-import streamlit as st 
+import streamlit as st
+from salaries.utils.helpers import get_salaries_df, read_textfile
+from salaries.utils.constants import MARKDOWN_PATH
 
-st.markdown("# DASHBOARD")
+
+def dashboard_layout():
+    st.markdown("# Salaries dashboard")
+    st.markdown(read_textfile(MARKDOWN_PATH / "salaries_dashboard_description.md"))
+    st.dataframe(get_salaries_df())
+
+
+if __name__ == "__main__":
+    dashboard_layout()
