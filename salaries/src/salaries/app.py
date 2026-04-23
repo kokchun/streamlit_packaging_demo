9c03e3b1916c8a46528182cfc38f9e87d13ef281
kokchun <kokchungiang@gmail.com>
2026-04-23 10:51:19 +0200

    pages init

--- salaries/src/salaries/app.py
+++ salaries/src/salaries/app.py
@@ -1,7 +1,9 @@
 import streamlit as st 
 
 pages = [
-    st.Page("pages/home.py", title = "Home")
+    st.Page("pages/home.py", title = "Home"),
+    st.Page("pages/raw_data.py", title = "Raw data"),
+    st.Page("pages/dashboard.py", title = "Dashboard"),
 ]
 
 pg = st.navigation(pages)
